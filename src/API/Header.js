737dae7cf072b6e28317caa45b9f2242174fcf9a
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-10-27 10:45:52 +0000

    sfadfd

---
+++ src/API/Header.js
@@ -0,0 +1,14 @@
+
+
+
+
+function Header() {
+    return ( 
+        <div>
+            <h1>MYPROMOSPHERE API</h1>
+
+        </div>
+    )
+}
+
+export default Header 
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Api from './API/Api';
+import Header from './API/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App ">
+      <Header />
+      
+
+      <Api/>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
